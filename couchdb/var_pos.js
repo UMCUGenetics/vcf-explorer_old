7addf082cc57c198f74907a86acdfe2b39878abf
Robert Ernst <rfernst@gmail.com>
2015-02-03 16:17:18 +0100

    Variant position map and reduce view

---
+++ couchdb/var_pos.js
@@ -0,0 +1,17 @@
+// default samples
+// /_design/default/_view/var_pos
+
+function(doc) {
+  for (pos_i in doc.BODY){
+    pos = doc.BODY[pos_i];
+    for (pos_sample_i in pos.samples) {
+      pos_sample = pos.samples[pos_sample_i]
+      emit([pos.chr,pos.pos,pos.alt],1);
+    }
+  }
+}
+
+// Reduce
+function (key, values) {
+    return sum(values);
+}
