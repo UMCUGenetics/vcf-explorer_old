f41425ca5176327e3e59328a99793df545d9ca8f
Robert Ernst <rfernst@gmail.com>
2015-02-24 14:40:24 +0100

    Update var_pos function

--- couchdb/views/var_pos.js
+++ couchdb/views/var_pos.js
@@ -6,7 +6,14 @@ function(doc) {
     pos = doc.BODY[pos_i];
     for (pos_sample_i in pos.samples) {
       pos_sample = pos.samples[pos_sample_i]
-      emit([pos.chr,pos.pos,pos.ref,pos.alt],1);
+
+      if (pos_sample.GT == "0/0"){
+        emit([pos.chr,pos.pos,pos.ref,pos.alt],[2,0]);
+      } else if (pos_sample.GT == "1/0"){
+        emit([pos.chr,pos.pos,pos.ref,pos.alt],[1,1]);
+      } else if (pos_sample.GT == "1/1"){
+        emit([pos.chr,pos.pos,pos.ref,pos.alt],[0,2]);
+      }
     }
   }
 }
@@ -14,4 +21,14 @@ function(doc) {
 // Reduce
 function (key, values) {
     return sum(values);
+}
+
+function(key, values) {
+  ref_count = 0
+  alt_count = 0
+  for (i in values){
+    ref_count += values[i][0]
+    alt_count += values[i][1]
+  }
+  return [ref_count,alt_count]
 }
